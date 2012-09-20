af305f9ad0563fc27ec83bf6193c60da2387521a
Konrad Reiche <konrad.reiche@gmail.com>
2012-09-20 15:38:36 +0200

    Format unique characters question
    I have reformatted the comments in the file which deals with the unique
    characters problem.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- strings/UniqueCharacters.py
+++ strings/UniqueCharacters.py
@@ -1,43 +1,38 @@
 '''
-Question: Implement an algorithm to determine if a string has all
-unique characters. What if you cannot use additional data structures?
-'''
+Question
 
-'''
+        Implement an algorithm to determine if a string has all unique characters. What
+        if you cannot use additional data structures?
+
 Solution 1
 
-Compare every character of the string with every other character of the
-string.
+        Compare every character of the string with every other character of the
+        string.
 
-Time Complexity O(n^2)
-Space Complexity O(1)
-'''
+        Time Complexity O(n^2)
+        Space Complexity O(1)
 
-'''
 Solution 2
 
-If it is allowed to modify the input string a sorting algorithm could be
-applied and then check whether neighboring characters are identical.
+        If it is allowed to modify the input string a sorting algorithm could be
+        applied and then check whether neighboring characters are identical.
 
 
-Time Complexity O(n log n)
-Space Complexity O(1)
-'''
+        Time Complexity O(n log n)
+        Space Complexity O(1)
 
-'''
 Solution 3
 
-Dependent on the string encoding there is a different size of maximum
-possible characeters. If there are more characters in the string than can
-be encodied the result is false.
+        Dependent on the string encoding there is a different size of maximum
+        possible characeters. If there are more characters in the string than can
+        be encodied the result is false.
 
-A boolean array allocated with the maximum size of possible characeters is
-used as a hash map to find out whether a character was already used.
+        A boolean array allocated with the maximum size of possible characeters is
+        used as a hash map to find out whether a character was already used.
 
-Time Complexity O(n)
-Space Complexity O(1)
+        Time Complexity O(n)
+        Space Complexity O(1)
 '''
-
 def hasUniqueCharacters(string, bits):
 
         maximum = 2 ** bits
@@ -61,9 +56,9 @@ print hasUniqueCharacters('Konrad', 8)
 '''
 Solution 4
 
-The required space of solution 3 can be reduced by using a bit vector
-instead of boolean array.
+        The required space of solution 3 can be reduced by using a bit vector
+        instead of boolean array.
 
-Time Complexity O(n)
-Space Complexity O(1)
+        Time Complexity O(n)
+        Space Complexity O(1)
 '''
