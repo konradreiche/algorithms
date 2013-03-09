5adcda10dcd316327626a670593e194f2f3bd3a2
Konrad Reiche <konrad.reiche@gmail.com>
2013-03-09 22:26:29 +0100

    Fix indention of tree implementation
    Just fixed some indention differences in the module implementing the
    tree data structure.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- trees/Tree.py
+++ trees/Tree.py
@@ -1,6 +1,6 @@
 import unittest
 
-'''
+"""
 Question
 
         Write a simple binary search tree.
@@ -10,63 +10,65 @@ Solution
         For the implementation tree nodes are used. The root is stored as
         an attribute in the tree data structure. If it is null the tree is
         empty.
-'''
+"""
+
+
 class TreeNode(object):
 
-        def __init__(self, data):
-                self.data = None
-                self.left = None
-                self.right = None
+    def __init__(self, data):
+        self.data = None
+        self.left = None
+        self.right = None
+
 
 class Tree(object):
 
-        def __init__(self):
-                self.root = None
+    def __init__(self):
+        self.root = None
 
-        def insert(self, data):
-                
-                '''Time Complexity O(n)'''
-                
-                new = TreeNode(data)
-                current = self.root
-                
-                if current is None:
-                        self.root = new
+    def insert(self, data):
+        """Time Complexity O(n)"""
+
+        new = TreeNode(data)
+        current = self.root
+
+        if current is None:
+            self.root = new
+            return
+
+            while True:
+                if data < current.data:
+                    if current.left is None:
+                        current.left = new
                         return
+                    else:
+                        current = current.left
+                elif data == current.data:
+                    return
+                else:
+                    if current.right is None:
+                        current.right = new
+                        return
+                    else:
+                        current = current.right
 
-                while True:
-                        if data < current.data:
-                                if current.left is None:
-                                        current.left = new
-                                        return
-                                else:
-                                        current = current.left
-                        elif data == current.data:
-                                return
-                        else:
-                                if current.right is None:
-                                        current.right = new
-                                        return
-                                else:
-                                        current = current.right
 
 class TestTree(unittest.TestCase):
 
-        def setUp(self):
-                self.tree = Tree()
+    def setUp(self):
+        self.tree = Tree()
 
-        def testInsert(self):
+    def testInsert(self):
 
-                try:
-                        self.tree.insert(11)
-                        self.tree.insert(7)
-                        self.tree.insert(13)
-                        self.tree.insert(5)
-                        self.tree.insert(17)
-                        self.tree.insert(3)
+        try:
+            self.tree.insert(11)
+            self.tree.insert(7)
+            self.tree.insert(13)
+            self.tree.insert(5)
+            self.tree.insert(17)
+            self.tree.insert(3)
+        except Exception, e:
+            self.fail(e)
 
-                except Exception as e:
-                        self.fail(e)
-                        
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
