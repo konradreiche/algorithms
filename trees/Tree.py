f0230c505a05d82335f04a12afffce38f9efe6e1
Konrad Reiche <konrad.reiche@gmail.com>
2013-03-09 22:36:38 +0100

    Add pre-, in- and postorder traversal
    I added the recursive variants of the preorder, inorder and postorder
    traversal algorithms. The iterative variants would be more interesting.
    I will add them later.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- trees/Tree.py
+++ trees/Tree.py
@@ -16,7 +16,7 @@ Solution
 class TreeNode(object):
 
     def __init__(self, data):
-        self.data = None
+        self.data = data
         self.left = None
         self.right = None
 
@@ -25,6 +25,27 @@ class Tree(object):
 
     def __init__(self):
         self.root = None
+
+    def preorder(self, node):
+        if not node:
+            return
+        # visit(node)
+        self.preorder(node.left)
+        self.preorder(node.right)
+
+    def inorder(self, node):
+        if not node:
+            return
+        self.inorder(node.left)
+        # visit(node)
+        self.inorder(node.right)
+
+    def postorder(self, node):
+        if not node:
+            return
+        self.postorder(node.left)
+        self.postorder(node.right)
+        # visit(node)
 
     def insert(self, data):
         """Time Complexity O(n)"""
@@ -36,21 +57,21 @@ class Tree(object):
             self.root = new
             return
 
-            while True:
-                if data < current.data:
-                    if current.left is None:
-                        current.left = new
-                        return
-                    else:
-                        current = current.left
-                elif data == current.data:
+        while True:
+            if data < current.data:
+                if current.left is None:
+                    current.left = new
                     return
                 else:
-                    if current.right is None:
-                        current.right = new
-                        return
-                    else:
-                        current = current.right
+                    current = current.left
+            elif data == current.data:
+                return
+            else:
+                if current.right is None:
+                    current.right = new
+                    return
+                else:
+                    current = current.right
 
 
 class TestTree(unittest.TestCase):
@@ -69,6 +90,7 @@ class TestTree(unittest.TestCase):
             self.tree.insert(3)
         except Exception, e:
             self.fail(e)
+
 
 if __name__ == '__main__':
     unittest.main()
